c0f985e82776d682abc9ff2f6e433c4776840d33
長崎宏太朗 <kmny03@gmail.com>
2021-08-05 11:58:37 +0000

    first commit

--- practice.php
+++ practice.php
@@ -32,19 +32,19 @@ $b = 3;
 $c = "3";
 var_dump($a == $b);
 $name="N.koutarou";
-if ($name="N.koutarou"){
-    echo "私は あなたの名前 です";
+if ($name=="N.koutarou"){
+    echo "私は"."$name"."です";
 }else{
-    echo "あなたの名前ではありません";
+    echo "あなたの名前は"."$name"."ありません";
 }
 $total=0;
-for($i=0;$i<=1000;$i++){
+for($i=0;$i<=10000;$i++){
     $total +=$i;
 }
 echo $total;
 $fruits= array("apple","orange","strawberry","cherry","banana");
-foreach($fruits as $fruits){
-    echo $fruits;
+foreach($fruits as $fruit){
+    echo $fruit;
     echo ".";
 }
 $start = 1;
@@ -52,5 +52,6 @@ $end = 100;
 for($i = $start; $i <=$end; $i++){
 if($i % 5 == 0){
     echo $i;
+    echo"\n";
   }
 }
